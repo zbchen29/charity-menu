590244235ade5be04e0a326711e1c6843948cef9
zhenbang <zbchen29@gmail.com>
2019-09-08 16:12:08 -0400

    Fixed drag swap bug; added list view

--- src/App.js
+++ src/App.js
@@ -8,7 +8,92 @@ import img2 from './images/img2.jpeg';
 import img3 from './images/img3.jpeg';
 import img4 from './images/img4.jpeg';
 
+let title = "Sample Item";
+let desc = "This is the sample description box containing information about image, which is pictured above.";
+let count = 11;
+let items = [
+    {image: img1, title:title+" 1", text:desc},
+    {image: img1, title:title+" 2", text:desc},
+    {image: img1, title:title+" 3", text:desc},
+    {image: img1, title:title+" 4", text:desc},
+    {image: img1, title:title+" 5", text:desc},
+    {image: img1, title:title+" 6", text:desc},
+    {image: img1, title:title+" 7", text:desc},
+    {image: img1, title:title+" 8", text:desc},
+    {image: img1, title:title+" 9", text:desc},
+    {image: img1, title:title+" 10", text:desc},
+    {image: img1, title:title+" 11", text:desc}
+]
+
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            editMode : false,
+            listMode : false,
+            items : items,
+            dragStartIndex : null,
+        }
+
+        this.createItems = this.createItems.bind(this);
+        this.toggleEditMode = this.toggleEditMode.bind(this);
+        this.toggleViewMode = this.toggleViewMode.bind(this);
+        this.dragStartHandler = this.dragStartHandler.bind(this);
+        this.dragDropHandler = this.dragDropHandler.bind(this);
+    }
+
+    // Renders the list of items
+    createItems() {
+        let cardItems = [];
+        console.log("start");
+        console.log("length: " + this.state.items.length);
+        for (let i=0; i<this.state.items.length; i++) {
+            cardItems.push(
+                <Item
+                key={i}
+                index={i}
+                image={this.state.items[i].image}
+                title={this.state.items[i].title}
+                text={this.state.items[i].text}
+                edit={this.state.editMode}
+                list={this.state.listMode}
+                dropHandler={this.dragDropHandler}
+                dragStartHandler={this.dragStartHandler}/>
+            );
+        }
+
+        return (
+            <div className="row justify-content-left mx-auto p-4">
+                {cardItems}
+            </div>
+        )
+    }
+
+    toggleEditMode() {
+        this.setState({editMode : !this.state.editMode});
+    }
+
+    toggleViewMode() {
+        this.setState({listMode : !this.state.listMode});
+    }
+
+    dragStartHandler(index) {
+        console.log(index);
+        this.setState({dragStartIndex : index});
+    }
+
+    dragDropHandler(index) {
+        console.log(index);
+        let key = index;
+        let items = this.state.items;
+        let tempItem = items[key];
+
+        items[key] = this.state.items[this.state.dragStartIndex];
+        items[this.state.dragStartIndex] = tempItem;
+
+        this.setState({items: items, dragStartIndex: null});
+    }
+
     render () {
         return (
             <div>
@@ -17,26 +102,14 @@ class App extends Component {
                 </div>
                 <div className="main-box mx-auto rounded-lg">
                     <div className="toolbar rounded-top text-right">
-                        <button className="txt-cabin btn btn-clear btn-lg">
+                        <button className="txt-cabin btn btn-clear btn-lg" onClick={this.toggleViewMode}>
                             View
                         </button>
-                        <button className="txt-cabin btn btn-clear btn-lg">
-                            Edit
+                        <button className="txt-cabin btn btn-clear btn-lg" onClick={this.toggleEditMode}>
+                            { this.state.editMode ? "Save" : "Edit" }
                         </button>
                     </div>
-                    <div className="row justify-content-left mx-auto p-4">
-                        <Item image={img1} title="Image 1" text="This is the sample description box containing information about image 1, which is pictured above."/>
-                        <Item image={img2} title="Image 2" text="This is the sample description box containing information about image 2, which is pictured above."/>
-                        <Item image={img3} title="Image 3" text="This is the sample description box containing information about image 3, which is pictured above."/>
-                        <Item image={img4} title="Image 4" text="This is the sample description box containing information about image 4, which is pictured above."/>
-                        <Item image={img1} title="Image 1" text="This is the sample description box containing information about image 1, which is pictured above."/>
-                        <Item image={img2} title="Image 2" text="This is the sample description box containing information about image 2, which is pictured above."/>
-                        <Item image={img3} title="Image 3" text="This is the sample description box containing information about image 3, which is pictured above."/>
-                        <Item image={img4} title="Image 4" text="This is the sample description box containing information about image 4, which is pictured above."/>
-                        <Item image={img1} title="Image 1" text="This is the sample description box containing information about image 1, which is pictured above."/>
-                        <Item image={img2} title="Image 2" text="This is the sample description box containing information about image 2, which is pictured above."/>
-                        <Item image={img3} title="Image 3" text="This is the sample description box containing information about image 3, which is pictured above."/>
-                    </div>
+                    {this.createItems()}
                 </div>
                 <div className="footer txt-cabin">
                     Information and Contact
--- src/components/Item.js
+++ src/components/Item.js
@@ -2,20 +2,52 @@ import React, { Component } from 'react';
 import './Item.css';
 
 class Item extends Component {
+    constructor(props) {
+        super(props)
+    }
+
     render() {
-        return (
-            <div className="card item-card">
-                <img className="card-img-top card-img" src={this.props.image}/>
-                <div className="card-body p-3">
-                    <div className="card-title txt-cabin txt-md">
-                        {this.props.title}
-                    </div>
-                    <div className="card-text txt-cabin txt-sm">
-                        {this.props.text}
+
+        if (this.props.list) {
+            return (
+                <div
+                className={"item-row" + (this.props.edit ? " editing" : "")}
+                draggable={this.props.edit}
+                onDragOver={(e)=>e.preventDefault()}
+                onDrop={(e)=>this.props.dropHandler(this.props.index)}
+                onDragStart={(e)=>this.props.dragStartHandler(this.props.index)}>
+                    <img className="col-4 item-list-img" draggable={false} src={this.props.image} alt="card"/>
+                    <div className="col-8 p-3">
+                        <div className="card-title txt-cabin txt-md">
+                            {this.props.title}
+                        </div>
+                        <div className="card-text txt-cabin txt-sm">
+                            {this.props.text}
+                        </div>
                     </div>
                 </div>
-            </div>
-        )
+            )
+        }
+        else {
+            return (
+                <div
+                className={"card item-card" + (this.props.edit ? " editing" : "")}
+                draggable={this.props.edit}
+                onDragOver={(e)=>e.preventDefault()}
+                onDrop={(e)=>this.props.dropHandler(this.props.index)}
+                onDragStart={(e)=>this.props.dragStartHandler(this.props.index)}>
+                    <img className="card-img-top item-card-img" draggable={false} src={this.props.image} alt="card"/>
+                    <div className="card-body p-3">
+                        <div className="card-title txt-cabin txt-md">
+                            {this.props.title}
+                        </div>
+                        <div className="card-text txt-cabin txt-sm">
+                            {this.props.text}
+                        </div>
+                    </div>
+                </div>
+            )
+        }
     }
 }
 
